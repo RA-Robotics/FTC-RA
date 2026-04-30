9376ac1e7c299258fc9fb7c558fad685f4f7b5d5
zain <zuqadri@icloud.com>
2026-04-30 15:59:44 -0500

    drive file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "Tank Drive TeleOp", group = "TeleOp")
+public class drive  extends LinearOpMode {
+
+    private DcMotor leftDriveMotor;
+    private DcMotor rightDriveMotor;
+
+    @Override
+    public void runOpMode() {
+
+        // Initialize motors
+        leftDriveMotor = hardwareMap.get(DcMotor.class, "leftMotor");
+        rightDriveMotor = hardwareMap.get(DcMotor.class, "rightMotor");
+
+        // Reverse one side so both wheels move forward together
+        leftDriveMotor.setDirection(DcMotor.Direction.REVERSE);
+        rightDriveMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        // Make sure motors don't try to use encoders
+        leftDriveMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightDriveMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        telemetry.addLine("Ready to run");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            // Tank drive controls
+            double leftPower = -gamepad1.left_stick_y;
+            double rightPower = -gamepad1.right_stick_y;
+
+            // Apply power
+            leftDriveMotor.setPower(leftPower);
+            rightDriveMotor.setPower(rightPower);
+
+            // Telemetry for debugging
+            telemetry.addData("Left Power", leftPower);
+            telemetry.addData("Right Power", rightPower);
+            telemetry.update();
+        }
+    }
+}
